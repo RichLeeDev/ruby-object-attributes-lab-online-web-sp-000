5b698539db8b4b1a9a0debc4f5a76f9e2baf1b4d
Richard Lee <richlee81192@gmail.com>
2019-09-26 23:01:16 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(persons_name)
+    @person_name = persons_name
+  end 
+  
+  def name
+    @person_name 
+  end
+  
+  def job=(jobs)
+    @job = jobs
+  end
+  
+  def job
+    @job 
+  end 
+end 
